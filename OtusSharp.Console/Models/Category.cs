60167f0abb27dd428cdb32585edc989db470d636
Vasiliy Voronko <mail@voronko.dev>
2023-08-19 16:29:07 +0300

    Add models

---
+++ OtusSharp.Console/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace OtusSharp.Console.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    
+    public required string Title { get; set; }
+}
---
+++ OtusSharp.Console/Models/Offer.cs
@@ -0,0 +1,13 @@
+﻿namespace OtusSharp.Console.Models;
+
+public class Offer
+{
+    public int Id { get; set; }
+
+    public required User Author { get; set; }
+    public required Category Category { get; set; }
+
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public float Cost { get; set; }
+}
